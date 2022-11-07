8d7dac1841077812ef3268c371162fa7b097bc46
adityak-git <70607267+adityak-git@users.noreply.github.com>
2022-11-07 12:04:42 -0500

    dashboard backend (needs to be connected to getters)

--- app/src/main/java/com/example/healthapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/healthapp/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,19 @@
 package com.example.healthapp.ui.dashboard
 
+import android.icu.number.IntegerWidth
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.healthapp.MainActivity
+import com.example.healthapp.R
 import com.example.healthapp.databinding.FragmentDashboardBinding
+import com.example.healthapp.databinding.FragmentHomeBinding
+import com.example.healthapp.ui.home.HomeViewModel
 
 class DashboardFragment : Fragment() {
 
@@ -28,10 +34,26 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        /*val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
-        }
+        }*/
+        var activity: MainActivity = MainActivity();
+        val yes = activity.YDAY
+
+
+
+        val eatProgress: Float = ((yes.getCalsEaten().toFloat() / yes.getCalsGoal().toFloat())*100.toFloat())
+       kotlin.math.round(eatProgress);
+        System.out.println(eatProgress);
+       var inteatProg = eatProgress .toInt();
+        var pview : ProgressBar=binding.progressBar2
+        pview.setProgress(inteatProg)
+        val burned = yes.getMealCals(0,"Waffles")
+        var tv : TextView= binding.pbarText
+        tv.text= inteatProg.toString()+"%"
+        //var x = getString(R.string.str1)
+        //x="nah"
         return root
     }
 
