23f173953736fada7eca87a50df6c6d30514e7a0
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2022-11-07 20:35:51 -0500

    questionaire page

---
+++ app/src/main/java/com/example/healthapp/ui/notifications/RegisterUserActivity.kt
@@ -0,0 +1,79 @@
+package com.example.healthapp.ui.notifications
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.example.healthapp.R
+
+class RegisterUserActivity : AppCompatActivity() {
+
+    private lateinit var mGenderRadioButtonGroup : RadioGroup
+    private lateinit var mUserContinueButton : Button
+
+    private lateinit var mUserNameEdit : EditText
+    private lateinit var mUserAgeEdit : EditText
+    private lateinit var mUserHeightFtEdit : EditText
+    private lateinit var mUserHeightInEdit : EditText
+    private lateinit var mUserWeightEdit : EditText
+
+    private val gender: Gender
+        get() {
+
+            when (mGenderRadioButtonGroup!!.checkedRadioButtonId) {
+                R.id.genderMButton -> {
+                    return Gender.Male
+                }
+                R.id.genderFButton -> {
+                    return Gender.Female
+                }
+                else -> {
+                    return Gender.Other
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+
+        // Bar Title
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.title = "Create Profile"
+        }
+
+        mGenderRadioButtonGroup = findViewById(R.id.genderRadioGroup)
+        mUserContinueButton = findViewById(R.id.userContinueButton)
+        mUserNameEdit = findViewById(R.id.userNameEdit)
+        mUserAgeEdit = findViewById(R.id.userAgeEdit)
+        mUserHeightFtEdit = findViewById(R.id.userHeightFtEdit)
+        mUserHeightInEdit = findViewById(R.id.userHeightInEdit)
+        mUserWeightEdit = findViewById(R.id.userWeightEdit)
+
+        // Save user data and move to home page
+        mUserContinueButton.setOnClickListener {
+            val name = mUserNameEdit.text.toString()
+            val age = mUserAgeEdit.text.toString().toIntOrNull()
+            val heightFt = mUserHeightFtEdit.text.toString().toIntOrNull()
+            val heightIn = mUserHeightInEdit.text.toString().toIntOrNull()
+            val weight = mUserWeightEdit.text.toString().toIntOrNull()
+            val selectedGender = this.gender.ordinal
+
+            // Value checks
+            if (name.equals("") || age == null || heightFt == null || heightIn == null || weight == null
+                || age <= 0 || heightFt < 0 || heightIn < 0 || heightIn >= 12 || weight <= 0) {
+                Toast.makeText(applicationContext, "Please make sure all of your data is valid.",
+                    Toast.LENGTH_LONG).show()
+            } else {
+                val user = User(name, age, selectedGender, heightFt, heightIn, weight)
+                User.saveUser(applicationContext, user)
+
+//                val startHome = Intent(this, HomeActivity::class.java)
+//                startActivity(startHome)
+            }
+        }
+    }
+}
