ab6b580007570b83955d254b31e842cc24c45c99
Ryan Brown <ryan.allen.empires@gmail.com>
2022-11-07 22:52:08 -0500

    Connected Files and Changed Top App Bar

---
+++ app/src/main/java/com/example/healthapp/ui/dashboard/AddActivity.kt
@@ -0,0 +1,66 @@
+package com.example.healthapp.ui.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
+import java.io.File
+import android.content.Context
+import android.util.Log
+import com.example.healthapp.R
+import java.io.IOException
+import java.nio.charset.Charset
+
+class AddActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_activity)
+
+        // Bar Title
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.title = "Add Activity"
+        }
+
+        val nameOfActivity = findViewById<EditText>(R.id.activityName)
+        val duration = findViewById<EditText>(R.id.duration)
+        val date = findViewById<EditText>(R.id.activityDate)
+        val weights = findViewById<EditText>(R.id.weight)
+        val reps = findViewById<EditText>(R.id.reps)
+        val button = findViewById<Button>(R.id.SubmitActivity)
+
+        val spinner: Spinner = findViewById(R.id.exerciseType)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.exercise_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
+
+        button.setOnClickListener {
+            val name = nameOfActivity.text.toString()
+            val durationString = duration.text.toString().toIntOrNull()
+            val weightString = weights.text.toString().toIntOrNull()
+            val repString = reps.text.toString().toIntOrNull()
+            val spinnerSelect = spinner.selectedItem.toString()
+            val dateString = date.text.toString()
+
+            if (name.equals("") || dateString.equals("") || durationString == null || weightString == null
+                || repString == null || spinnerSelect.equals("")
+            ) {
+                Toast.makeText(
+                    applicationContext, "There is an invalid value in your activity entry.",
+                    Toast.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
+            }
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/healthapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/healthapp/ui/home/HomeFragment.kt
@@ -27,11 +27,11 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//
+//        val textView: TextView = binding.
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
